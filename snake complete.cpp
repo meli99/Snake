e7ab66215ba3e79c60e3ff675066d932af2489b9
Meli <53483691+meli99@users.noreply.github.com>
2019-08-31 15:19:24 -0600

    Update snake complete.cpp

--- snake complete.cpp
+++ snake complete.cpp
@@ -90,3 +90,31 @@ bool game_over()
     }
     
     return true;}
+int main()
+
+{
+//    OcultaCursor();
+    
+    pintar();
+    gotoxy(xc, yc); printf("%c", 4);
+    
+    while(tecla != ESC && game_over())
+    {
+        borrar_cuerpo();
+        guardar_posicion();
+        dibujar_cuerpo();
+        comida();
+        teclear();
+        teclear();
+        
+        if(dir == 1) y--;
+        if(dir == 2) y++;
+        if(dir == 3) x++;
+        if(dir == 4) x--;
+        
+        Sleep(velocidad);
+    }
+    pintar();
+
+    return 0;
+}
